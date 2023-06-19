82bf139c53991bc22f4f6be3bf65946f2dbb25f7
Larry Athey <panhandleponics@larryathey.com>
2023-06-18 21:39:49 -0600

    Update CZ_Outdoor_Monitor.ino

--- CZ_Outdoor_Monitor/CZ_Outdoor_Monitor.ino
+++ CZ_Outdoor_Monitor/CZ_Outdoor_Monitor.ino
@@ -53,7 +53,7 @@ BH1750 lightMeter;
 Arduino_DataBus *bus = new Arduino_ESP32LCD8(7 /* DC */, 6 /* CS */, 8 /* WR */, 9 /* RD */, 39 /* D0 */, 40 /* D1 */, 41 /* D2 */, 42 /* D3 */,
                                              45 /* D4 */, 46 /* D5 */, 47 /* D6 */, 48 /* D7 */);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, 5 /* RST */, 0 /* rotation */, true /* IPS */, 170 /* width */, 320 /* height */, 35 /* col offset 1 */,
-                                      0 /* row offset 1 */, 35 /* col offset 2 */, 0 /* row offset 2 */);
+                                           0 /* row offset 1 */, 35 /* col offset 2 */, 0 /* row offset 2 */);
 
 // Comment out the following 4 lines if you want to use DHCP (see setup section as well)
 IPAddress staticIP(10,20,30,161);
@@ -158,7 +158,7 @@ void SetFG(float Temp) {
     gfx->setTextColor(GREEN);
   } else if ((Temp > 78) && (Temp <= 85)) {
     gfx->setTextColor(YELLOW);
-  } else if ((Temp > 88) && (Temp <= 95)) {
+  } else if ((Temp > 85) && (Temp <= 95)) {
     gfx->setTextColor(MAGENTA);
   } else {
     gfx->setTextColor(RED);
